3c9d42428302fd16d4743c14b0842425c4d08b5c
Marius <marius@example.com>
2022-03-15 10:21:17 +0200

    uzd

---
+++ js/main.js
@@ -0,0 +1,36 @@
+const users = [
+  {
+    id: 1,
+    username: 'Bob',
+    password: 'secret123',
+    department: 'A',
+    avatar: '1.jpg',
+    online: false,
+  },
+  {
+    id: 2,
+    username: 'Jill',
+    password: 'secret456',
+    department: 'B',
+    avatar: '2.jpg',
+    online: true,
+  },
+  {
+    id: 3,
+    username: 'James',
+    password: 'secret789',
+    department: 'B',
+    avatar: '3.jpg',
+    online: true,
+  },
+  {
+    id: 4,
+    username: 'Mike',
+    password: 'secret111',
+    department: 'C',
+    avatar: '4.jpg',
+    online: false,
+  },
+];
+
+// 1. parasyti funkcija kuri atrenka visus online esancius vartotojus
