cbdafc0c3acc3de2093f2a4690ffbe0a80295840
Marius <marius@example.com>
2022-03-15 11:31:05 +0200

    make img

--- js/main.js
+++ js/main.js
@@ -32,9 +32,73 @@ const users = [
     online: false,
   },
 ];
-
+console.table(users);
 // 1. parasyti funkcija kuri atrenka visus online esancius vartotojus
-
+// const online = users.filter((uObj) => uObj.online === true);
+const online = onlineUsers();
+// console.table(online);
+function onlineUsers() {
+  return users.filter((uObj) => uObj.online === true);
+}
 // 2. su funkcija atrinkti B ir C departameto userius
+function departmentUsers(arr) {
+  const filteredUsers = arr.filter((uObj) => uObj.department === 'B' || uObj.department === 'C');
+  return filteredUsers;
+}
+// array.includes()
+// extra
+const bcUsers = departmentUsers(users, ['B', 'C']);
+const aUsers = departmentUsers(users, ['A']);
+console.table(bcUsers);
 
 // 3. parasyti funkcija kuri sugeneruoja htmle nuotrauku galerija is uzer paveikleliu, su username pavadinimu
+// {
+//   /* <figure>
+//       <img src="pic_trulli.jpg" alt="Trulli">
+//       <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+//     </figure> */
+// }
+// 3.1 parasyti funkcija kuri gavusi toki objekta
+const u1 = {
+  id: 3,
+  username: 'James',
+  password: 'secret789',
+  department: 'B',
+  avatar: '3.jpg',
+  online: true,
+};
+// pagamina
+// {
+//   /* <figure>
+//       <img src="3.jpg" alt="James">
+//       <figcaption>Image of James</figcaption>
+//     </figure> */
+// }
+// makeMeFig(u1)
+
+// 3.2 susikuriam div su klase 'grid'. ir tada funkcija makeCards(arr, dest) kuri suka cikla per arr
+// ir gamina korteles su auksciau aprasyta funkcija (3.1)
+
+// 4. parasyti funkcija kuriai paduodam id ir ji grazina objekta kurio id sutampa su duotu. jei toks nerandamas tai grazina
+// {
+//   found: false;
+//   msg: 'user not found';
+// }
+// function findById(){}
+// findById(100)
+
+// 5. Parasyti funkcija kuriai paduodam username ir password.
+// 5.1 jei pasword ir username sutampa tai iskonsolinam loggin success
+// 5.2 jei pasword ir username nesutampa tai iskonsolinam loggin fail
+// 5.3 jei pasword ir username sutampa tai padarom vartotoja kad jis butu online
+
+function makeImg(userObj) {
+  const imgEl = document.createElement('img');
+  imgEl.className = 'user-img';
+  imgEl.src = `img/${userObj.avatar}`;
+  imgEl.alt = `Image of ${userObj.username}`;
+  document.body.append(imgEl);
+}
+
+makeImg(users[0]);
+makeImg(users[1]);
